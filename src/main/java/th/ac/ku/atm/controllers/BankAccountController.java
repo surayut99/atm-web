3dd3062bf67b3511db383118f2b4a920bc7c160d
puen <surayut.bo@ku.th>
2020-10-01 17:24:50 +0700

    add deposit and withdraw

--- src/main/java/th/ac/ku/atm/controllers/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controllers/BankAccountController.java
@@ -1,5 +1,7 @@
 package th.ac.ku.atm.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import th.ac.ku.atm.models.BankAccount;
 import th.ac.ku.atm.services.BankAccountService;
@@ -56,6 +59,24 @@ public class BankAccountController {
     public String deleteAccount(@PathVariable int id, Model model) {
         BankAccount account = bankAccountService.getBankAccount(id);
         bankAccountService.deleteBankAccount(account);
+
+        return "redirect:/bankaccount";
+    }
+
+    @PostMapping("/deposit/{id}")
+    public String deposit(@PathVariable int id, @RequestParam Map<String, String> map, Model model) {
+        BankAccount account = bankAccountService.getBankAccount(id);
+        account.setBalance(account.getBalance() + Integer.parseInt(map.get(id + "")));
+        bankAccountService.editBankAccount(account);
+
+        return "redirect:/bankaccount";
+    }
+
+    @PostMapping("/withdraw/{id}")
+    public String withdraw(@PathVariable int id, @RequestParam Map<String, String> map, Model model) {
+        BankAccount account = bankAccountService.getBankAccount(id);
+        account.setBalance(account.getBalance() - Integer.parseInt(map.get(id + "")));
+        bankAccountService.editBankAccount(account);
 
         return "redirect:/bankaccount";
     }
